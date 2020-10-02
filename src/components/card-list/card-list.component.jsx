6d08cd50e036794f0a7b58726324154b3750f94f
Luka-dot <79lukask@gmail.com>
2020-10-02 11:13:51 -0400

    moving card-list set up

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import { Card } from '../card/card.component';
 import './card.styles.css';
 
-export const CardList = (props) => {
+export const CardList = (props) => (
 
-    return(
     <div className='card-list'>
         {
-          props.contacts.map(contact => <h1 key={contact.id}>{contact.name}</h1>)
+          props.cardContacts.map(contact => (
+            <Card key={contact.id} singleContact={contact} />
+            ))
         }
     </div>
-    )
-};
+);
 
 export default CardList;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Card = (props) => (
+
+    <div>
+        <h1>
+            {
+                props.singleContact.name
+            }
+        </h1>
+    </div>
+)
