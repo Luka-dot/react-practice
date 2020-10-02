54d673e08db4a7133f3f433a888f151626437ab5
Luka-dot <79lukask@gmail.com>
2020-10-02 13:44:00 -0400

    card componennt email added

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,8 +6,11 @@ export const Card = (props) => (
 
     <div className='card-container' >
         <img alt='contatc' src={`https://robohash.org/${props.singleContact.id}?set=set5&size=180x180`} />
-        <h1>
+        <h2>
             { props.singleContact.name }
-        </h1>
+        </h2>
+        <h2>
+            { props.singleContact.email }
+        </h2>
     </div>
 )
