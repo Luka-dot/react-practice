844a53b3b2895f72854cf60c2ce17213aef49ef2
Luka-dot <79lukask@gmail.com>
2020-10-02 11:51:20 -0400

    card component styles

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './card.styles.css';
+
 export const Card = (props) => (
 
-    <div>
+    <div className='clrd-container' >
         <h1>
             {
                 props.singleContact.name
